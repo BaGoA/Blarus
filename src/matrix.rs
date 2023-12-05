74a29dbd8ca3827905397e1c9472bfff4886f10c
Bastian Gonzalez Acevedo <bastiangonzalezacevedo@gmail.com>
2023-12-05 19:32:17 +0100

    Add Matrix structure and implement a constructor

--- src/matrix.rs
+++ src/matrix.rs
@@ -15,6 +15,7 @@ enum Ordering {
 struct StorageOrder {
     stride_row: usize,
     stride_col: usize,
+    order: Ordering,
 }
 
 impl StorageOrder {
@@ -26,10 +27,12 @@ impl StorageOrder {
             Ordering::RowMajor => Self {
                 stride_row: nb_cols,
                 stride_col: 1,
+                order,
             },
             Ordering::ColumnMajor => Self {
                 stride_row: 1,
                 stride_col: nb_rows,
+                order,
             },
         }
     }
@@ -38,6 +41,33 @@ impl StorageOrder {
     /// from row index anf colunm index
     fn index(&self, row_id: usize, col_id: usize) -> usize {
         return row_id * self.stride_row + col_id * self.stride_col;
+    }
+}
+
+/// Matrix
+/// The data is stored in contiguous memory vector
+/// according to ordering chosen
+struct Matrix<T> {
+    nb_rows: usize,
+    nb_cols: usize,
+    storage_order: StorageOrder,
+    data: Vec<T>,
+}
+
+impl<T> Matrix<T>
+where
+    T: Default,
+{
+    fn new(nb_rows: usize, nb_cols: usize, order: Ordering) -> Self {
+        let mut data: Vec<T> = Vec::new();
+        data.resize_with(nb_rows * nb_cols, Default::default);
+
+        return Self {
+            nb_rows,
+            nb_cols,
+            storage_order: StorageOrder::new(nb_rows, nb_cols, order),
+            data,
+        };
     }
 }
 
@@ -69,5 +99,17 @@ mod tests {
 
         storage_order = StorageOrder::new(nb_rows, nb_cols, Ordering::ColumnMajor);
         assert_eq!(storage_order.index(2, 1), 2 + nb_rows);
+    }
+
+    #[test]
+    fn test_matrix_new() {
+        let nb_rows: usize = 3;
+        let nb_cols: usize = 3;
+
+        let matrix: Matrix<i32> = Matrix::new(nb_rows, nb_cols, Ordering::RowMajor);
+
+        assert_eq!(matrix.nb_rows, nb_rows);
+        assert_eq!(matrix.nb_cols, nb_cols);
+        assert_eq!(matrix.data.len(), nb_rows * nb_cols);
     }
 }
