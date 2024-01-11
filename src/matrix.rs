fb8bf04f1cbc3b3dbf49f459503e7fbeeb573976
BaGoA <bastiangonzalezacevedo@gmail.com>
2024-01-11 20:14:31 +0100

    view module (#1)
    * Replace Strides structure by Accessor structure in view module
    
    * Implement View structure with Index trait implementation
    
    * Implement ViewMut structure with Index and IndexMut trait implementation
    
    * Improve code documentation
    
    * View and mutable view on matrix

--- src/matrix.rs
+++ src/matrix.rs
@@ -1,59 +1,14 @@
 use std::ops::{Index, IndexMut};
 
-/// Matrix ordering
-/// The matrix is stored in contiguous memory vector.
-/// Then, there are two way to store a matrix:
-///     - according to row, row major ordering
-///     - according to column, column major ordering
-#[derive(Clone, Copy, Debug, PartialEq)]
-pub enum Ordering {
-    RowMajor,
-    ColumnMajor,
-}
-
-/// Matrix storage order
-/// These structure contain strides along row and column
-/// that we need to apply to matrix indexes (i, j) to obtain the
-/// index of data in vector which store matrix data
-struct StorageOrder {
-    stride_row: usize,
-    stride_col: usize,
-    order: Ordering,
-}
-
-impl StorageOrder {
-    // Matrix storage order constructor
-    // nb_rows and nb_cols correspond respectively to number of rows and columns of matrix
-    // order is matrix ordering
-    fn new(nb_rows: usize, nb_cols: usize, order: Ordering) -> Self {
-        match order {
-            Ordering::RowMajor => Self {
-                stride_row: nb_cols,
-                stride_col: 1,
-                order,
-            },
-            Ordering::ColumnMajor => Self {
-                stride_row: 1,
-                stride_col: nb_rows,
-                order,
-            },
-        }
-    }
-
-    /// Compute index of data in vector which store matrix data
-    /// from row index and colunm index
-    fn index(&self, row_id: usize, col_id: usize) -> usize {
-        return row_id * self.stride_row + col_id * self.stride_col;
-    }
-}
+use super::view::{Accessor, View, ViewMut};
 
 /// Matrix
-/// The data is stored in contiguous memory vector
-/// according to ordering chosen
+/// This structure contains number of rows and number of columns of matrix, an accessor
+/// to get memory position of elements and a vector to store matrix data
 pub struct Matrix<T> {
     nb_rows: usize,
     nb_cols: usize,
-    storage_order: StorageOrder,
+    accessor: Accessor,
     data: Vec<T>,
 }
 
@@ -61,17 +16,28 @@ impl<T> Matrix<T>
 where
     T: Default,
 {
-    // Matrix constructor
-    // nb_rows and nb_cols correspond respectively to number of rows and columns of matrix
-    // order is matrix ordering
-    pub fn new(nb_rows: usize, nb_cols: usize, order: Ordering) -> Self {
+    // Create a row-major matrix from number of rows and columns of matrix
+    pub fn new_row_major(nb_rows: usize, nb_cols: usize) -> Self {
         let mut data: Vec<T> = Vec::new();
         data.resize_with(nb_rows * nb_cols, Default::default);
 
         return Self {
             nb_rows,
             nb_cols,
-            storage_order: StorageOrder::new(nb_rows, nb_cols, order),
+            accessor: Accessor::new(nb_cols, 1),
+            data,
+        };
+    }
+
+    // Create a column-major matrix from number of rows and columns of matrix
+    pub fn new_column_major(nb_rows: usize, nb_cols: usize) -> Self {
+        let mut data: Vec<T> = Vec::new();
+        data.resize_with(nb_rows * nb_cols, Default::default);
+
+        return Self {
+            nb_rows,
+            nb_cols,
+            accessor: Accessor::new(1, nb_rows),
             data,
         };
     }
@@ -85,31 +51,97 @@ where
     pub fn nb_cols(&self) -> usize {
         return self.nb_cols;
     }
+}
 
-    /// Get matrix ordering
-    pub fn order(&self) -> Ordering {
-        return self.storage_order.order;
+/// View parameters
+/// This structure contains this indexes of first element of view
+/// and number of rows and number of colunm that we want
+pub struct ViewParameters {
+    start_row: usize,
+    start_col: usize,
+    nb_rows: usize,
+    nb_cols: usize,
+}
+
+impl ViewParameters {
+    pub fn new(start_row: usize, start_col: usize, nb_rows: usize, nb_cols: usize) -> Self {
+        return ViewParameters {
+            start_row,
+            start_col,
+            nb_rows,
+            nb_cols,
+        };
     }
 }
 
-/// Implementation of Index trait for Matrix
-/// This allows to read the matrix element at (index of row, index of column) position
-/// like this let element: f32 = matrix[(0, 2)];
+impl<'a, T> Matrix<T> {
+    /// Get full view of matrix
+    pub fn full_view(&'a self) -> View<'a, T> {
+        return View::new(
+            self.nb_rows,
+            self.nb_cols,
+            self.accessor.clone(),
+            self.data.as_slice(),
+        );
+    }
+
+    /// Get full mutable view of matrix
+    pub fn full_view_mut(&'a mut self) -> ViewMut<'a, T> {
+        return ViewMut::new(
+            self.nb_rows,
+            self.nb_cols,
+            self.accessor.clone(),
+            self.data.as_mut_slice(),
+        );
+    }
+
+    /// Get view on part of matrix
+    pub fn view(&'a self, params: ViewParameters) -> View<'a, T> {
+        return View::new(
+            params.nb_rows,
+            params.nb_cols,
+            Accessor::new_with_offset(
+                self.accessor.stride_row,
+                self.accessor.stride_col,
+                params.start_row,
+                params.start_col,
+            ),
+            self.data.as_slice(),
+        );
+    }
+
+    /// Get mutable view on part of matrix
+    pub fn view_mut(&'a mut self, params: ViewParameters) -> ViewMut<'a, T> {
+        return ViewMut::new(
+            params.nb_rows,
+            params.nb_cols,
+            Accessor::new_with_offset(
+                self.accessor.stride_row,
+                self.accessor.stride_col,
+                params.start_row,
+                params.start_col,
+            ),
+            self.data.as_mut_slice(),
+        );
+    }
+}
+
 impl<T> Index<(usize, usize)> for Matrix<T> {
     type Output = T;
 
+    /// This allows to read the matrix element at (index of row, index of column) position
+    /// like this let element: f32 = matrix[(0, 2)];
     fn index(&self, index: (usize, usize)) -> &Self::Output {
-        let id: usize = self.storage_order.index(index.0, index.1);
+        let id: usize = self.accessor.index(index.0, index.1);
         return self.data.index(id);
     }
 }
 
-/// Implementation of IndexMut trait for Matrix
-/// This allows to write an value in matrix at (index of row, index of column) position
-/// like this matrix[(0, 2)] = 3.1415;
 impl<T> IndexMut<(usize, usize)> for Matrix<T> {
+    /// This allows to write an value in matrix at (index of row, index of column) position
+    /// like this matrix[(0, 2)] = 3.1415;
     fn index_mut(&mut self, index: (usize, usize)) -> &mut Self::Output {
-        let id: usize = self.storage_order.index(index.0, index.1);
+        let id: usize = self.accessor.index(index.0, index.1);
         return self.data.index_mut(id);
     }
 }
@@ -119,37 +151,11 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_storage_order_new() {
-        let nb_rows: usize = 2;
-        let nb_cols: usize = 3;
-
-        let mut storage_order = StorageOrder::new(nb_rows, nb_cols, Ordering::RowMajor);
-        assert_eq!(storage_order.stride_row, nb_cols);
-        assert_eq!(storage_order.stride_col, 1);
-
-        storage_order = StorageOrder::new(nb_rows, nb_cols, Ordering::ColumnMajor);
-        assert_eq!(storage_order.stride_row, 1);
-        assert_eq!(storage_order.stride_col, nb_rows);
-    }
-
-    #[test]
-    fn test_storage_order_index() {
+    fn test_matrix_new_row_major() {
         let nb_rows: usize = 3;
-        let nb_cols: usize = 3;
+        let nb_cols: usize = 4;
 
-        let mut storage_order = StorageOrder::new(nb_rows, nb_cols, Ordering::RowMajor);
-        assert_eq!(storage_order.index(1, 2), nb_cols + 2);
-
-        storage_order = StorageOrder::new(nb_rows, nb_cols, Ordering::ColumnMajor);
-        assert_eq!(storage_order.index(2, 1), 2 + nb_rows);
-    }
-
-    #[test]
-    fn test_matrix_new() {
-        let nb_rows: usize = 3;
-        let nb_cols: usize = 3;
-
-        let matrix: Matrix<i32> = Matrix::new(nb_rows, nb_cols, Ordering::RowMajor);
+        let matrix: Matrix<i32> = Matrix::new_row_major(nb_rows, nb_cols);
 
         assert_eq!(matrix.nb_rows, nb_rows);
         assert_eq!(matrix.nb_cols, nb_cols);
@@ -157,43 +163,293 @@ mod tests {
     }
 
     #[test]
-    fn test_matrix_dimensions_accessors() {
-        let nb_rows: usize = 3;
+    fn test_matrix_new_column_major() {
+        let nb_rows: usize = 4;
         let nb_cols: usize = 3;
 
-        let matrix: Matrix<i32> = Matrix::new(nb_rows, nb_cols, Ordering::RowMajor);
+        let matrix: Matrix<i32> = Matrix::new_column_major(nb_rows, nb_cols);
+
+        assert_eq!(matrix.nb_rows, nb_rows);
+        assert_eq!(matrix.nb_cols, nb_cols);
+        assert_eq!(matrix.data.len(), nb_rows * nb_cols);
+    }
+
+    #[test]
+    fn test_matrix_dimensions_access() {
+        let nb_rows: usize = 5;
+        let nb_cols: usize = 3;
+
+        let matrix: Matrix<i32> = Matrix::new_row_major(nb_rows, nb_cols);
 
         assert_eq!(matrix.nb_rows(), nb_rows);
         assert_eq!(matrix.nb_cols(), nb_cols);
     }
 
     #[test]
-    fn test_matrix_order_accessor() {
+    fn test_matrix_row_major_data_access() {
         let nb_rows: usize = 3;
         let nb_cols: usize = 3;
 
-        let order: Ordering = Ordering::ColumnMajor;
-        let matrix: Matrix<i32> = Matrix::new(nb_rows, nb_cols, order);
+        let mut matrix: Matrix<i32> = Matrix::new_row_major(nb_rows, nb_cols);
 
-        assert_eq!(matrix.order(), order);
+        let data_ref: Vec<i32> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9];
+        matrix.data = data_ref.clone();
+
+        assert_eq!(matrix[(0, 0)], data_ref[0]);
+        assert_eq!(matrix[(0, 1)], data_ref[1]);
+        assert_eq!(matrix[(0, 2)], data_ref[2]);
+        assert_eq!(matrix[(1, 0)], data_ref[3]);
+        assert_eq!(matrix[(1, 1)], data_ref[4]);
+        assert_eq!(matrix[(1, 2)], data_ref[5]);
+        assert_eq!(matrix[(2, 0)], data_ref[6]);
+        assert_eq!(matrix[(2, 1)], data_ref[7]);
+        assert_eq!(matrix[(2, 2)], data_ref[8]);
+
+        matrix[(2, 1)] = 43;
+        assert_eq!(matrix[(2, 1)], 43);
     }
 
     #[test]
-    fn test_matrix_index_accessors() {
+    fn test_matrix_column_major_data_access() {
         let nb_rows: usize = 3;
         let nb_cols: usize = 3;
 
-        let mut matrix: Matrix<i32> = Matrix::new(nb_rows, nb_cols, Ordering::RowMajor);
+        let mut matrix: Matrix<i32> = Matrix::new_column_major(nb_rows, nb_cols);
 
-        let value: i32 = 1;
+        let data_ref: Vec<i32> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9];
+        matrix.data = data_ref.clone();
 
-        matrix[(0, 0)] = value;
-        assert_eq!(matrix[(0, 0)], value);
+        assert_eq!(matrix[(0, 0)], data_ref[0]);
+        assert_eq!(matrix[(1, 0)], data_ref[1]);
+        assert_eq!(matrix[(2, 0)], data_ref[2]);
+        assert_eq!(matrix[(0, 1)], data_ref[3]);
+        assert_eq!(matrix[(1, 1)], data_ref[4]);
+        assert_eq!(matrix[(2, 1)], data_ref[5]);
+        assert_eq!(matrix[(0, 2)], data_ref[6]);
+        assert_eq!(matrix[(1, 2)], data_ref[7]);
+        assert_eq!(matrix[(2, 2)], data_ref[8]);
 
-        matrix[(2, 1)] = value;
-        assert_eq!(matrix[(2, 1)], value);
+        matrix[(2, 1)] = 43;
+        assert_eq!(matrix[(2, 1)], 43);
+    }
 
-        matrix[(1, 2)] = value;
-        assert_eq!(matrix[(1, 2)], value);
+    #[test]
+    fn test_matrix_row_major_full_view() {
+        let nb_rows: usize = 3;
+        let nb_cols: usize = 3;
+
+        let mut matrix: Matrix<i32> = Matrix::new_row_major(nb_rows, nb_cols);
+
+        let data_ref: Vec<i32> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9];
+        matrix.data = data_ref.clone();
+
+        let view: View<i32> = matrix.full_view();
+
+        assert_eq!(view[(0, 0)], data_ref[0]);
+        assert_eq!(view[(0, 1)], data_ref[1]);
+        assert_eq!(view[(0, 2)], data_ref[2]);
+        assert_eq!(view[(1, 0)], data_ref[3]);
+        assert_eq!(view[(1, 1)], data_ref[4]);
+        assert_eq!(view[(1, 2)], data_ref[5]);
+        assert_eq!(view[(2, 0)], data_ref[6]);
+        assert_eq!(view[(2, 1)], data_ref[7]);
+        assert_eq!(view[(2, 2)], data_ref[8]);
+    }
+
+    #[test]
+    fn test_matrix_column_major_full_view() {
+        let nb_rows: usize = 3;
+        let nb_cols: usize = 3;
+
+        let mut matrix: Matrix<i32> = Matrix::new_column_major(nb_rows, nb_cols);
+
+        let data_ref: Vec<i32> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9];
+        matrix.data = data_ref.clone();
+
+        let view: View<i32> = matrix.full_view();
+
+        assert_eq!(view[(0, 0)], data_ref[0]);
+        assert_eq!(view[(1, 0)], data_ref[1]);
+        assert_eq!(view[(2, 0)], data_ref[2]);
+        assert_eq!(view[(0, 1)], data_ref[3]);
+        assert_eq!(view[(1, 1)], data_ref[4]);
+        assert_eq!(view[(2, 1)], data_ref[5]);
+        assert_eq!(view[(0, 2)], data_ref[6]);
+        assert_eq!(view[(1, 2)], data_ref[7]);
+        assert_eq!(view[(2, 2)], data_ref[8]);
+    }
+
+    #[test]
+    fn test_matrix_row_major_full_mutable_view() {
+        let nb_rows: usize = 3;
+        let nb_cols: usize = 3;
+
+        let mut matrix: Matrix<i32> = Matrix::new_row_major(nb_rows, nb_cols);
+
+        let data_ref: Vec<i32> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9];
+        matrix.data = data_ref.clone();
+
+        let factor: i32 = 3;
+
+        {
+            let mut view: ViewMut<i32> = matrix.full_view_mut();
+
+            view[(1, 2)] *= factor;
+            view[(2, 1)] *= factor;
+        }
+
+        assert_eq!(matrix[(0, 0)], data_ref[0]);
+        assert_eq!(matrix[(0, 1)], data_ref[1]);
+        assert_eq!(matrix[(0, 2)], data_ref[2]);
+        assert_eq!(matrix[(1, 0)], data_ref[3]);
+        assert_eq!(matrix[(1, 1)], data_ref[4]);
+        assert_eq!(matrix[(1, 2)], factor * data_ref[5]);
+        assert_eq!(matrix[(2, 0)], data_ref[6]);
+        assert_eq!(matrix[(2, 1)], factor * data_ref[7]);
+        assert_eq!(matrix[(2, 2)], data_ref[8]);
+    }
+
+    #[test]
+    fn test_matrix_column_major_full_view_mut() {
+        let nb_rows: usize = 3;
+        let nb_cols: usize = 3;
+
+        let mut matrix: Matrix<i32> = Matrix::new_column_major(nb_rows, nb_cols);
+
+        let data_ref: Vec<i32> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9];
+        matrix.data = data_ref.clone();
+
+        let factor: i32 = 3;
+
+        {
+            let mut view: ViewMut<i32> = matrix.full_view_mut();
+
+            view[(1, 0)] *= factor;
+            view[(2, 1)] *= factor;
+        }
+
+        assert_eq!(matrix[(0, 0)], data_ref[0]);
+        assert_eq!(matrix[(1, 0)], factor * data_ref[1]);
+        assert_eq!(matrix[(2, 0)], data_ref[2]);
+        assert_eq!(matrix[(0, 1)], data_ref[3]);
+        assert_eq!(matrix[(1, 1)], data_ref[4]);
+        assert_eq!(matrix[(2, 1)], factor * data_ref[5]);
+        assert_eq!(matrix[(0, 2)], data_ref[6]);
+        assert_eq!(matrix[(1, 2)], data_ref[7]);
+        assert_eq!(matrix[(2, 2)], data_ref[8]);
+    }
+
+    #[test]
+    fn test_matrix_row_major_view() {
+        let nb_rows: usize = 4;
+        let nb_cols: usize = 4;
+
+        let mut matrix: Matrix<i32> = Matrix::new_row_major(nb_rows, nb_cols);
+
+        let data_ref: Vec<i32> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+        matrix.data = data_ref.clone();
+
+        let view: View<i32> = matrix.view(ViewParameters::new(1, 1, 2, 2));
+
+        assert_eq!(view[(0, 0)], data_ref[5]);
+        assert_eq!(view[(0, 1)], data_ref[6]);
+        assert_eq!(view[(1, 0)], data_ref[9]);
+        assert_eq!(view[(1, 1)], data_ref[10]);
+    }
+
+    #[test]
+    fn test_matrix_column_major_view() {
+        let nb_rows: usize = 4;
+        let nb_cols: usize = 4;
+
+        let mut matrix: Matrix<i32> = Matrix::new_column_major(nb_rows, nb_cols);
+
+        let data_ref: Vec<i32> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+        matrix.data = data_ref.clone();
+
+        let view: View<i32> = matrix.view(ViewParameters::new(1, 1, 2, 2));
+
+        assert_eq!(view[(0, 0)], data_ref[5]);
+        assert_eq!(view[(0, 1)], data_ref[9]);
+        assert_eq!(view[(1, 0)], data_ref[6]);
+        assert_eq!(view[(1, 1)], data_ref[10]);
+    }
+
+    #[test]
+    fn test_matrix_row_major_view_mut() {
+        let nb_rows: usize = 4;
+        let nb_cols: usize = 4;
+
+        let mut matrix: Matrix<i32> = Matrix::new_row_major(nb_rows, nb_cols);
+
+        let data_ref: Vec<i32> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+        matrix.data = data_ref.clone();
+
+        let factor: i32 = 3;
+
+        {
+            let mut view: ViewMut<i32> = matrix.view_mut(ViewParameters::new(1, 1, 2, 2));
+
+            view[(0, 0)] *= factor;
+            view[(0, 1)] *= factor;
+            view[(1, 0)] *= factor;
+            view[(1, 1)] *= factor;
+        }
+
+        assert_eq!(matrix[(0, 0)], data_ref[0]);
+        assert_eq!(matrix[(0, 1)], data_ref[1]);
+        assert_eq!(matrix[(0, 2)], data_ref[2]);
+        assert_eq!(matrix[(0, 3)], data_ref[3]);
+        assert_eq!(matrix[(1, 0)], data_ref[4]);
+        assert_eq!(matrix[(1, 1)], factor * data_ref[5]);
+        assert_eq!(matrix[(1, 2)], factor * data_ref[6]);
+        assert_eq!(matrix[(1, 3)], data_ref[7]);
+        assert_eq!(matrix[(2, 0)], data_ref[8]);
+        assert_eq!(matrix[(2, 1)], factor * data_ref[9]);
+        assert_eq!(matrix[(2, 2)], factor * data_ref[10]);
+        assert_eq!(matrix[(2, 3)], data_ref[11]);
+        assert_eq!(matrix[(3, 0)], data_ref[12]);
+        assert_eq!(matrix[(3, 1)], data_ref[13]);
+        assert_eq!(matrix[(3, 2)], data_ref[14]);
+        assert_eq!(matrix[(3, 3)], data_ref[15]);
+    }
+
+    #[test]
+    fn test_matrix_column_major_view_mut() {
+        let nb_rows: usize = 4;
+        let nb_cols: usize = 4;
+
+        let mut matrix: Matrix<i32> = Matrix::new_column_major(nb_rows, nb_cols);
+
+        let data_ref: Vec<i32> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+        matrix.data = data_ref.clone();
+
+        let factor: i32 = 3;
+
+        {
+            let mut view: ViewMut<i32> = matrix.view_mut(ViewParameters::new(1, 1, 2, 2));
+
+            view[(0, 0)] *= factor;
+            view[(0, 1)] *= factor;
+            view[(1, 0)] *= factor;
+            view[(1, 1)] *= factor;
+        }
+
+        assert_eq!(matrix[(0, 0)], data_ref[0]);
+        assert_eq!(matrix[(1, 0)], data_ref[1]);
+        assert_eq!(matrix[(2, 0)], data_ref[2]);
+        assert_eq!(matrix[(3, 0)], data_ref[3]);
+        assert_eq!(matrix[(0, 1)], data_ref[4]);
+        assert_eq!(matrix[(1, 1)], factor * data_ref[5]);
+        assert_eq!(matrix[(2, 1)], factor * data_ref[6]);
+        assert_eq!(matrix[(3, 1)], data_ref[7]);
+        assert_eq!(matrix[(0, 2)], data_ref[8]);
+        assert_eq!(matrix[(1, 2)], factor * data_ref[9]);
+        assert_eq!(matrix[(2, 2)], factor * data_ref[10]);
+        assert_eq!(matrix[(3, 2)], data_ref[11]);
+        assert_eq!(matrix[(0, 3)], data_ref[12]);
+        assert_eq!(matrix[(1, 3)], data_ref[13]);
+        assert_eq!(matrix[(2, 3)], data_ref[14]);
+        assert_eq!(matrix[(3, 3)], data_ref[15]);
     }
 }
